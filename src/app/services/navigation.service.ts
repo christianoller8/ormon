0a643e3f646428513a83f1ed8fdc0b70d7252b00
Christian Oller <christianoller.dev@gmail.com>
2023-04-09 14:19:15 +0200

    Breadcrumbs, header and footer added and styled

---
+++ src/app/services/navigation.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from "@angular/core";
+import { Location } from "@angular/common";
+import { Router, NavigationEnd } from "@angular/router";
+import { delay } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class NavigationService {
+  private history: string[] = [];
+
+  constructor(private router: Router, private location: Location) {}
+
+  public startSaveHistory(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.history.push(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  public getHistory(): string[] {
+    return this.history;
+  }
+
+  public goBack(): void {
+    this.history.pop();
+    if (this.history.length > 0) {
+      this.router.navigateByUrl(this.history.pop()!);
+    } else {
+      this.router.navigateByUrl("/");
+    }
+  }
+
+  public getPreviousUrl(): string {
+    if (this.history.length > 0) {
+      return this.history[this.history.length - 2];
+    }
+
+    return "";
+  }
+
+  public async syncWithBreadcrumbs() {
+    await delay(50);
+    const currentUrl = this.router.url;
+    this.history.pop();
+
+    while (
+      this.history[this.history.length - 1] != currentUrl &&
+      this.history.length > 0
+    ) {
+      this.history.pop();
+    }
+  }
+}
